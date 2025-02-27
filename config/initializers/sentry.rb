c7f8a96636d015b0b39e73d17d19404175dc0651
Casper Schuijt <casper@cschuijt.nl>
2025-02-27 18:32:39 +0000

    Sentry init

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+if ENV["SENTRY_DSN"] && !ENV["SENTRY_DSN"].blank?
+  Sentry.init do |config|
+    config.breadcrumbs_logger = [ :active_support_logger ]
+    config.dsn = ENV.fetch("SENTRY_DSN")
+    config.enable_tracing = true
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    config.traces_sample_rate = ENV.fetch("SENTRY_TRACE_SAMPLE_RATE") { 1.0 }&.to_f
+
+    # Profile all requests, for funsies
+    config.profiles_sample_rate = ENV.fetch("SENTRY_PROFILE_SAMPLE_RATE") { 0.05 }&.to_f
+  end
+end
