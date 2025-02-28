1079ea46993e7509b7ca906f151d7a1582bc6bd0
Casper Schuijt <casper@cschuijt.nl>
2025-02-28 12:25:43 +0000

    pages controller, bulma

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
+  root "pages#home"
 
   # Authentication routes
   get "/auth/marvin/callback", to: "sessions#create"
