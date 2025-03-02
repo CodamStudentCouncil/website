76fd7d74771d74567f5e5f7c9e1604f61fe6738e
Casper Schuijt <casper@cschuijt.nl>
2025-03-02 16:01:48 +0000

    add time zone support through env file

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module StudentCountil
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = ENV.fetch("TIME_ZONE") { "Europe/Amsterdam" }
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
