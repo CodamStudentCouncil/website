865d6cba5676a4b61f56b336431432bdee95720e
Casper Schuijt <casper@cschuijt.nl>
2025-03-07 13:57:29 +0000

    validations for election dates (closes #2)

--- app/models/election.rb
+++ app/models/election.rb
@@ -3,10 +3,30 @@ class Election < ApplicationRecord
   has_many :registrations, dependent: :destroy
   has_many :votes, dependent: :destroy
 
+  validates_presence_of :start_date, :end_date
+  validates :end_date, comparison: { greater_than: :start_date }
+
+
   def self.current
-    Election.where("start_date < ?", Time.zone.now.to_date)
-            .where("end_date > ?", Time.zone.now.to_date)
+    self.during_date(Time.zone.now.to_date)
+  end
+
+  def self.during_date(date)
+    Election.where("start_date < ?", date)
+            .where("end_date > ?", date)
             .limit(1)
             &.take
+  end
+
+  private
+
+  def no_election_during_date
+    if Election.during_date(self.start_date)
+      errors.add(:start_date, "cannot be during another election")
+    end
+
+    if Election.during_date(self.end_date)
+      errors.add(:end_date, "cannot be during another election")
+    end
   end
 end
