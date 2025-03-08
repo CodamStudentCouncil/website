5a68c098e29061a89cc2e00715fa210be4113015
Casper Schuijt <casper@cschuijt.nl>
2025-03-08 17:00:34 +0000

    support files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
 require 'rspec/rails'
+require_relative 'support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
