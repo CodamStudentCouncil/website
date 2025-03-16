3c271c90d03d035dc1aabfc0d0ddc32af4ab4648
Casper Schuijt <casper@cschuijt.nl>
2025-03-16 16:36:09 +0000

    preprod build

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
+    if Rails.env.preprod? && flash.now[:info].blank?
+      flash.now["info"] = "You are in the preproduction environment, running a development build. Some things may be broken, and changes you make will not affect the production environment."
+    end
     render layout: "seamless"
   end
 
