958a6dd93d29ddde2602115a2eab4d20f5ca556e
Casper Schuijt <casper@cschuijt.nl>
2025-03-18 15:03:04 +0000

    add methods on the Election model to support Registration cleanup

--- app/models/election.rb
+++ app/models/election.rb
@@ -20,6 +20,22 @@ class Election < ApplicationRecord
             &.take
   end
 
+  def in_progress?
+    Time.zone.now.to_date >= self.start_date && Time.zone.now.to_date <= self.end_date
+  end
+
+  def clean_up_registrations!
+    raise ElectionInProgressError,
+          "Cannot delete registrations while election is in progress" if self.in_progress?
+
+    self.transaction do
+      self.registrations.destroy_all
+    end
+  end
+
+  class ElectionInProgressError < StandardError
+  end
+
   private
 
   def no_election_during_date
--- spec/models/election_spec.rb
+++ spec/models/election_spec.rb
@@ -70,4 +70,82 @@ RSpec.describe Election, type: :model do
       expect(Election.current).to be_falsy
     end
   end
+
+  describe "in_progress?" do
+    before(:context) do
+      @today = Time.zone.now.to_date
+    end
+
+    it "should be true if the current date is between the start and end date" do
+      election = create(:election, start_date: @today - 3, end_date: @today + 4)
+      expect(election.in_progress?).to be_truthy
+    end
+
+    it "should be true if the current date is the start date" do
+      election = create(:election, start_date: @today, end_date: @today + 3)
+      expect(election.in_progress?).to be_truthy
+    end
+
+    it "should be true if the current date is the end date" do
+      election = create(:election, start_date: @today - 7, end_date: @today)
+      expect(election.in_progress?).to be_truthy
+    end
+
+    it "should be false if the current date is before the start date" do
+      election = create(:election, start_date: @today + 1, end_date: @today + 8)
+      expect(election.in_progress?).to be_falsy
+    end
+
+    it "should be false if the current date is after the end date" do
+      election = create(:election, start_date: @today - 8, end_date: @today - 1)
+      expect(election.in_progress?).to be_falsy
+    end
+  end
+
+  describe "clean_up_registrations!" do
+    context "with an in-progress election" do
+      before(:example) do
+        @election = create(:election, start_date: Time.zone.now.to_date - 3, end_date: Time.zone.now.to_date + 3)
+        create_list(:vote, 5, election: @election)
+        create_list(:registration, 5, election: @election)
+      end
+
+      it "should raise an error" do
+        expect { @election.clean_up_registrations! }
+          .to raise_error(Election::ElectionInProgressError)
+      end
+
+      it "should not remove registrations" do
+        expect { @election.clean_up_registrations! rescue Election::ElectionInProgressError }
+          .not_to change(@election, :registrations)
+      end
+    end
+
+    context "with a finished election" do
+      before(:example) do
+        @election = create(
+          :election,
+          start_date: Time.zone.now.to_date - 8,
+          end_date: Time.zone.now.to_date - 1
+        )
+        create_list(:vote, 5, election: @election)
+        create_list(:registration, 5, election: @election)
+      end
+
+      it "should reduce registration count to zero" do
+        expect { @election.clean_up_registrations! }
+          .to change(@election.registrations, :count).from(5).to(0)
+      end
+
+      it "should not change votes" do
+        expect { @election.clean_up_registrations! }
+          .not_to change { @election.votes }
+      end
+
+      it "should not change vote count" do
+        expect { @election.clean_up_registrations! }
+          .not_to change(@election.votes, :count)
+      end
+    end
+  end
 end
