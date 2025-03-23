a3b9ac606655bb7d4959ae6e5d091ffdaff8cdf4
Casper Schuijt <casper@cschuijt.nl>
2025-03-23 19:09:09 +0000

    fix missing method for current council validator, slight touchup on elections index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  include SessionsHelper
+  include SessionsHelper, ApplicationHelper
 
   private
 
